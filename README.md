c0767bfccd8cefd551ca7a6509fa23c51ac44b57
ReneRumetshofer <51208355+ReneRumetshofer@users.noreply.github.com>
2020-10-22 00:25:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# COVID-19 case visualization for Austria in Node.js
+## What's it about?
+This Node app accesses the official statistics of COVID-19 cases in Austria (per county level) via [ebimt.pro](https://www.ebimt.pro) COVID-19 API
+and generates SVG graphs based on this data.
+![Total cases](https://github.com/ReneRumetshofer/CovidVisualizationNode/blob/main/img/Amstetten_cumulated.png?raw=true "Total cases graph")
+![Active cases](https://github.com/ReneRumetshofer/CovidVisualizationNode/blob/main/img/Amstetten_active.png?raw=true "Active cases graph")
+
+## Get started
+- Register an account at the [ebimt.pro](https://www.ebimt.pro) COVID-19 API
+- Clone this repository and create a `.env` file in it. The following entries are needed:
+	- API_USERNAME: Username of the API user
+	- **OPTIONAL** (to not get queried for the API user's password every time):
+		- API_ACCESS_TOKEN: Any (previous) API access token
+		- API_REFRESH_TOKEN: The main API token for obtaining access tokens
+- Run `npm install`
+- Run the app: `npm start`
+- The graphs will be created in the folder!
+
+### Changing county (Bezirk)
+At the moment `visualize.js` needs to be changed: Set the constant `BEZIRK_TO_FETCH` to the name of the county that shall get visualized.
+
+## License / Terms of use
+Feel free to use, change or redistribute this code; no mentioning needed.
---
+++ visualize.js
@@ -0,0 +1,256 @@
+require('dotenv/config');
+const fetch = require('node-fetch');
+const readline = require("readline");
+const fs = require('fs');
+const vega = require('vega');
+
+// Routes
+const API_BASE = 'https://www.ebimt.pro:5000/';
+const USER_BASE = API_BASE + 'user/';
+
+// CONSTANTS
+const BEZIRK_TO_FETCH = 'Amstetten';
+const CUT_FACTOR = 3; // Determines how many data points are shown. If cut by two, only every 2nd dot will be graphed
+
+// Init console
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Check for connectivity
+fetch(API_BASE + 'info')
+.then(res => res.json())
+.then(async (json) => {
+    console.log("API Username: " + process.env.API_USERNAME);
+    
+    // Fetch tokens
+    var tokens;
+    if(!process.env.API_REFRESH_TOKEN || !process.env.API_ACCESS_TOKEN){
+        tokens = await obtainAPIKey().catch(err => {
+            console.log(err);
+            process.exit(1);
+        });
+        tokens = {accessToken: tokens.accessToken.token, refreshToken: tokens.refreshToken};
+    }
+    else{
+        tokenPairTmp = {accessToken: process.env.API_ACCESS_TOKEN, refreshToken: process.env.API_REFRESH_TOKEN};
+        tokens = await refreshKey(tokenPairTmp).catch(err => {
+            console.log(err);
+            process.exit(1);
+        });
+    }
+
+    // Fetch all Bezirke
+    var allBezirke = await fetchAllBezirke(tokens).catch(err => {
+        console.log(err);
+        process.exit(1);
+    });
+    allBezirke = allBezirke.results;
+    
+    // Fetch statistic entries
+    var bezirkModel;
+    for(let i = 0; i < allBezirke.length; i++) {
+        if(allBezirke[i].name === BEZIRK_TO_FETCH){
+            bezirkModel = allBezirke[i];
+            break;
+        }
+    }
+    var statisticEntries = await fetchAllStatisticEntriesForBezirk(tokens, bezirkModel.gkz).catch(err => {
+        console.log(err);
+        process.exit(1);
+    });
+    statisticEntries = statisticEntries.results;
+
+
+    /** Graph the statistics **/
+    // Prepare data sets
+    const totalCases = [];
+    const activeCases = [];
+    const totalDeaths = [];
+    const recoveredTotal = [];
+    const sevenDayIncidence = [];
+    for(let i = 0; i < statisticEntries.length; i++){
+        var date = new Date(statisticEntries[i].date);
+        totalCases.push({date: date, cases: statisticEntries[i].casesTotal});
+
+        var active = statisticEntries[i].casesTotal - statisticEntries[i].recoveredTotal - statisticEntries[i].deathsTotal;
+        activeCases.push({date: date, cases: active});
+
+        totalDeaths.push({date: date, cases: statisticEntries[i].deathsTotal});
+        recoveredTotal.push({date: date, cases: statisticEntries[i].recoveredTotal});
+        sevenDayIncidence.push({date: date, cases: statisticEntries[i].SevenDayIncidence})
+    }
+
+    // Write graphs (SVG)
+    writeGraph("Total COVID-19 cases in 'Bezirk " + bezirkModel.name + "' (absolute)", "COVID-19 cases (absolute)", totalCases, CUT_FACTOR,
+        bezirkModel.name + '_cumulated_cases.svg', () => console.log('Total graph written.'));
+    writeGraph("Active COVID-19 cases in 'Bezirk " + bezirkModel.name + "' (absolute)", "Active COVID-19 cases (absolute)", activeCases, CUT_FACTOR,
+        bezirkModel.name + '_active_cases.svg', () => console.log('Active graph written.'));
+    writeGraph("Total COVID-19 deaths in 'Bezirk " + bezirkModel.name + "' (absolute)", "COVID-19 deaths (absolute)", totalDeaths, CUT_FACTOR,
+        bezirkModel.name + '_total_deaths.svg', () => console.log('Deaths graph written.'));
+    writeGraph("COVID-19 7-day-incidence in 'Bezirk " + bezirkModel.name + "' (absolute)", "7-day-incidence", sevenDayIncidence, CUT_FACTOR,
+        bezirkModel.name + '_7_day_incidence.svg', () => console.log('7d incidence graph written.'));
+    writeGraph("Recovered COVID-19 patients in 'Bezirk " + bezirkModel.name + "' (absolute)", "COVID-19 recovered count (absolute)", recoveredTotal, CUT_FACTOR,
+        bezirkModel.name + '_recovered.svg', () => {
+            console.log('Recovered graph written.');
+            process.exit(0);
+        });
+})
+.catch(err => console.log('!! No connection possible' + err));
+
+// Writes a SVG graph.
+// title: Title above the whole graph
+// yTitle: Title of the y axis
+// data: Data to plot
+// cutFactor: Factor for cutting data to cut down on mark points in the graph
+// fileName: File name of generated graph (SVG)
+// callback: Parameterless callback function
+function writeGraph(title, yTitle, data, cutFactor, fileName, callback){
+    var chartDefinition = require('./chartDefinition.json');
+    chartDefinition.title.text = title;
+    chartDefinition.axes[0].title = yTitle;
+    chartDefinition.data[0].values = data;
+
+    // Cut original data according to passed factor
+    const cutData = [];
+    for(let i = 0; i < data.length / cutFactor; i++){
+        cutData.push(data[i * cutFactor]);
+    }
+    chartDefinition.data[1].values = cutData;
+
+    // Write graph
+    var view = new vega.View(vega.parse(chartDefinition)).renderer('none').initialize();
+    view.toSVG()
+    .then(svg => {
+        fs.writeFile(fileName, svg, () => callback());
+    })
+    .catch(err => {
+        console.log('Error while writing SVG graph:');
+        console.log(err);
+        throw err;
+    });
+}
+
+// Fetches all statistic entries for a given Bezirk
+async function fetchAllStatisticEntriesForBezirk(tokenPair, gkz) {
+    return new Promise(async (resolve, reject) => {
+        let status;
+        fetch(API_BASE + 'statisticEntry?gkz=' + gkz, { headers: {'Token': tokenPair.accessToken} })
+        .then(res => {
+            status = res.status;
+            return res.json();
+        })
+        .then(statisticEntryJson => {
+            if(!statisticEntryJson || status != 200)
+                return reject('Couldn\'t fetch statistic entries!');
+            else
+                return resolve(statisticEntryJson);
+        })
+        .catch(err => {
+            return reject(err);
+        });
+    });
+}
+
+// Fetches a Bezirk model. GKZ = Gemeindekennzahl
+async function fetchBezirk(tokenPair, gkz) {
+    return new Promise(async (resolve, reject) => {
+        let status;
+        fetch(API_BASE + 'bezirk/' + gkz, { headers: {'Token': tokenPair.accessToken} })
+        .then(res => {
+            status = res.status;
+            return res.json();
+        })
+        .then(bezirkJson => {
+            if(!bezirkJson || status != 200)
+                return reject('Couldn\'t fetch bezirk!');
+            else
+                return resolve(bezirkJson);
+        })
+        .catch(err => {
+            return reject(err);
+        });
+    });
+}
+
+// Fetches all Bezirk models. GKZ = Gemeindekennzahl
+async function fetchAllBezirke(tokenPair) {
+    return new Promise(async (resolve, reject) => {
+        let status;
+        fetch(API_BASE + 'bezirk', { headers: {'Token': tokenPair.accessToken} })
+        .then(res => {
+            status = res.status;
+            return res.json();
+        })
+        .then(bezirkJson => {
+            if(!bezirkJson || status != 200)
+                return reject('Couldn\'t fetch bezirke!');
+            else
+                return resolve(bezirkJson);
+        })
+        .catch(err => {
+            return reject(err);
+        });
+    });
+}
+
+// Takes in a tokenPair and refreshes the access token.
+async function refreshKey(tokenPair) {
+    return new Promise(async (resolve, reject) => {
+        let status;
+        fetch(USER_BASE + 'refreshToken', {method: 'POST',
+            body: JSON.stringify(tokenPair),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res => {
+            status = res.status;
+            return res.json();
+        })
+        .then(tokenJson => {
+            if(!tokenJson || status != 200)
+                return reject('!! Couldn\'t refresh tokens!');
+            else
+                return resolve({accessToken: tokenJson.accessToken.token, refreshToken: tokenPair.refreshToken});
+        })
+        .catch(err => {
+            return reject(err);
+        });
+    });
+}
+
+// Asks for the user's password, obtains tokens.
+async function obtainAPIKey() {
+    return new Promise(async (resolve, reject) => {
+        await rl.question("[*] API password: ", function(password) {
+            let status;
+            fetch(USER_BASE + 'login', {method: 'POST', 
+                body: JSON.stringify({
+                    email: process.env.API_USERNAME,
+                    password: password
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(res => {
+                status = res.status;
+                return res.json();
+            })
+            .then(tokenJson => {
+                console.log(status);
+                if(!tokenJson || tokenJson.error || status != 200)
+                    return reject('!! Invalid password - could not obtain API key!' + (tokenJson.error ? ' Error: ' + tokenJson.error : ''));
+
+                process.env['API_REFRESH_TOKEN'] = tokenJson.refreshToken;
+                process.env['API_ACCESS_TOKEN'] = tokenJson.accessToken.token;
+                return resolve(tokenJson);
+            })
+            .catch(err => {
+                return reject(err);
+            });
+        });
+    });
+}
